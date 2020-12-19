5c9ad4d428fcb6870bb749b44af949fed2c6bf90
adityakahb <aditya.bhakay@live.com>
2020-12-19 19:00:17 +0530

    Working on Event Detail page

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,19 @@ module.exports = (env, argv) => {
           }
         },
         {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: './src/assets/webfonts',
+                publicPath: '../../../../assets/webfonts'
+              }
+            }
+          ]
+        },
+        {
           test: /\.scss$/,
           use: [
             MiniCssExtractPlugin.loader,
