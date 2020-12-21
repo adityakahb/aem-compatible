a6e69af16f87f008158336cb32083063a9191a4d
adityakahb <aditya.bhakay@live.com>
2020-12-21 14:34:43 +0530

    Woking on Image colors

--- src/templates/_baseLayoutPage/clientlibs/scripts/index.es.js
+++ src/templates/_baseLayoutPage/clientlibs/scripts/index.es.js
@@ -2,13 +2,32 @@ import Vendors from './../../../../assets/scripts/_vendors.es';
 import SiteHeader from '../../../../components/siteheader/_index.es';
 import SiteFooter from '../../../../components/sitefooter/_index.es';
 import {_AddClass, _RemoveClass, _LandscapeCSSClass, _PortraitCSSClass, _GetElemDimensions} from './../../../../assets/scripts/_constants.es';
+import ColorThief from 'colorthief/dist/color-thief.mjs';
 
 let bodyElem;
 let bgElements;
 let bgElementDimension;
+let imgElem;
 
 class GlobalScripts {
   constructor() {}
+  imgLoadEvent() {
+    const colorThief = new ColorThief();
+    if ((bgElements || []).length > 0) {
+      bgElements.forEach(element => {
+        imgElem = element.querySelector('img');
+        if (imgElem) {
+          if (imgElem.complete) {
+            console.log('===============colorThief complete', colorThief.getColor(imgElem));
+          } else {
+            imgElem.addEventListener('load', function() {
+              console.log('===============colorThief load', colorThief.getColor(imgElem));
+            });
+          }
+        }
+      });
+    }
+  }
   reportWindowOrientation() {
     setTimeout(() => {
       if ((bgElements || []).length > 0) {
@@ -39,6 +58,7 @@ class GlobalScripts {
     bgElements = Array.prototype.slice.call(document.querySelectorAll('.bg-img-wrap') || []);
     this.reportWindowOrientation();
     window.addEventListener('resize', this.reportWindowOrientation, false);
+    this.imgLoadEvent();
 
     const siteheader = new SiteHeader();
     const vendors = new Vendors();
--- src/templates/search-results/clientlibs/scripts/index.js
+++ src/templates/search-results/clientlibs/scripts/index.js
@@ -84,6 +84,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "_LandscapeCSSClass": function() { return /* binding */ _LandscapeCSSClass; },
 /* harmony export */   "_PortraitCSSClass": function() { return /* binding */ _PortraitCSSClass; },
 /* harmony export */   "_DefaultCarouselOptions": function() { return /* binding */ _DefaultCarouselOptions; },
+/* harmony export */   "_GetAverageColor": function() { return /* binding */ _GetAverageColor; },
 /* harmony export */   "_GetElemDimensions": function() { return /* binding */ _GetElemDimensions; },
 /* harmony export */   "_StringTrim": function() { return /* binding */ _StringTrim; },
 /* harmony export */   "_ArrayCall": function() { return /* binding */ _ArrayCall; },
@@ -140,6 +141,57 @@ var _DefaultCarouselOptions = {
       }
     }
   }
+};
+var _GetAverageColor = function _GetAverageColor(imageElement) {
+  // Create the canavs element 
+  var canvas = document.createElement('canvas'),
+      // Get the 2D context of the canvas 
+  context = canvas.getContext && canvas.getContext('2d'),
+      imgData,
+      width,
+      height,
+      length,
+      // Define variables for storing 
+  // the individual red, blue and 
+  // green colors 
+  rgb = {
+    r: 0,
+    g: 0,
+    b: 0
+  },
+      // Define variable for the  
+  // total number of colors 
+  count = 0; // Set the height and width equal 
+  // to that of the canvas and the image 
+
+  height = canvas.height = imageElement.naturalHeight || imageElement.offsetHeight || imageElement.height;
+  width = canvas.width = imageElement.naturalWidth || imageElement.offsetWidth || imageElement.width; // Draw the image to the canvas 
+
+  context.drawImage(imageElement, 0, 0); // Get the data of the image 
+
+  imgData = context.getImageData(0, 0, width, height); // Get the length of image data object 
+
+  length = imgData.data.length;
+
+  for (var i = 0; i < length; i += 4) {
+    // Sum all values of red colour 
+    rgb.r += imgData.data[i]; // Sum all values of green colour 
+
+    rgb.g += imgData.data[i + 1]; // Sum all values of blue colour 
+
+    rgb.b += imgData.data[i + 2]; // Increment the total number of 
+    // values of rgb colours 
+
+    count++;
+  } // Find the average of red 
+
+
+  rgb.r = Math.floor(rgb.r / count); // Find the average of green 
+
+  rgb.g = Math.floor(rgb.g / count); // Find the average of blue 
+
+  rgb.b = Math.floor(rgb.b / count);
+  return rgb;
 };
 var _GetElemDimensions = function _GetElemDimensions(elem) {
   try {
